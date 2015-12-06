cc9484466d1587abacb144a8f1d9028a0a956209
zheka <zheka@MacBook-Pro-zheka.local>
2015-12-06 21:13:41 +0300

    update 2

--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,7 @@ router.get('/', rules.rules().isLogged, function(req, res) {
 router.post('/', function(req, res) {
     console.log(req.body.email);
     console.log(req.body.password);
-	couch.Users.findOneByView('_design/findOneByView/_view/findOneByView', req.body.email, function(err, user) {
+    couch.Users.findOneByView('_design/findOneByView/_view/findOneByView', req.body.email, function(err, user) {
         console.log(user.email);
         console.log(user.password);
         var error = "";
@@ -29,9 +29,8 @@ router.post('/', function(req, res) {
                 console.log("dgf");
                 res.render('auth', { title: "Chat Room - Login", error: error });
             } else {
-		if(req.body.password == user.password){
-            console.log("error if");
-                //if (bcrypt.crypt().compareHashSync(req.body.password, user.password)) {
+                if(req.body.password == user.password){
+                    console.log("error if");
                     req.session.user = user;
                     res.redirect('/room');
                 } else {
